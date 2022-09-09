d2320ce8519a2472c6b60496fd25a6822db75990
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-08 21:49:15 -0500

    ejercicio 9

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 addEventListener ("DOMContentLoaded", ()=>{
-    //Escribir un programa que evalúe la siguiente expresión: (a+7*c)/(b+2-a)+2*b
-    let numA=Number (prompt(`ingrese el numero`,10));
-    let numB=Number (prompt(`ingrese el numero`,12));
-    let numC=Number (prompt(`ingrese el numero`,15));
-    let num1=Number (prompt(`ingrese el numero`,2));
-    let num2=Number (prompt(`ingrese el numero`,7));
-    
-    let total=(numA+num1*numC)/(numB+num2-numA)+num2*numB
-    
-    //salida o entrega de resultados
-    
-    console.log("VALOR DE LA EXPRESION ",`${total}`);
+    //Escribir un programa que calcule el área y el volumen de un cilindro:A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)V = (PI * r2) * h
+    //Datos de entrada
+    let num1=number(prompt(`ingrese el numero`,2));
+    let num2=number(prompt(`ingrese el numero`,3.14));
+    let num3=number(prompt(`ingrese el numero`,4));
+    let num4=number(prompt(`ingrese el numero`,6));
+    //procesos y operaciones
+    let Area=(num1*(num2*num3**2))+((num1*num2*num3)*num4);
+    let Volumen=(num2*num3**2)*num4;
+    //salida y entrega de resultados
+    console.log("AREA DE CILINDRO",Area);
+    console.log("VOLUMEN DE CILINDRO",Volumen);
 })
