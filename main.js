4faa1960765f9abec8052269c0be4acb4f64b8eb
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-08 21:51:13 -0500

    ejercicio 10

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 addEventListener ("DOMContentLoaded", ()=>{
-    //Escribir un programa que calcule el área y el volumen de un cilindro:A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)V = (PI * r2) * h
-    //Datos de entrada
-    let num1=number(prompt(`ingrese el numero`,2));
-    let num2=number(prompt(`ingrese el numero`,3.14));
-    let num3=number(prompt(`ingrese el numero`,4));
-    let num4=number(prompt(`ingrese el numero`,6));
-    //procesos y operaciones
-    let Area=(num1*(num2*num3**2))+((num1*num2*num3)*num4);
-    let Volumen=(num2*num3**2)*num4;
-    //salida y entrega de resultados
-    console.log("AREA DE CILINDRO",Area);
-    console.log("VOLUMEN DE CILINDRO",Volumen);
+    //10. Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 +b * x + c, siendo X un valor constante.
+    //A*X**2+b*x+c siendo x un valor constante
+    //from math import sqrt 
+    let a=number(prompt(`ingrese el valor de (a):/n`));
+    let b=number(prompt(`ingrese el valor de (b):/n`));
+    let c=number(prompt(`ingrese el valor de (c):/n`));
+    let x=number(23);
+    //procesos
+    let pr=(b**2-4 *a*c);
+    if (pr<0);
+        console.log(`raiz imaginaria`);
+    
+        let x1=(-b+sqrt(b**2-(4*a*c)))/(2*a)
+        let x2=(-b-sqrt(b**2-(4*a*c)))/(2*a)
+    
+      console.log(` el valor de x1(raiz positiva)es igual a:`,x1)
+      console.log(` el valor de x2(raiz negativa) es igual a:`,x2)
+    
+                                                                                                                                                                                                                                                                                                                                                                                  
+    //salida o entrega de resultados
+      console.log(`el resultado re la raiz`)
 })
