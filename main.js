546e37a0505f572697d38a4a5f9af597267214d6
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-08 22:02:58 -0500

    ejercicio 11

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-addEventListener ("DOMContentLoaded", ()=>{
-    //10. Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 +b * x + c, siendo X un valor constante.
-    //A*X**2+b*x+c siendo x un valor constante
-    //from math import sqrt 
-    let a=number(prompt(`ingrese el valor de (a):/n`));
-    let b=number(prompt(`ingrese el valor de (b):/n`));
-    let c=number(prompt(`ingrese el valor de (c):/n`));
-    let x=number(23);
-    //procesos
-    let pr=(b**2-4 *a*c);
-    if (pr<0);
-        console.log(`raiz imaginaria`);
+addEventListener("DOMContentLoaded", ()=>{
+    // 11.Escribir un programa que calcule el volumen de un elipsoide V = (4/3) * PI * a * b *c
     
-        let x1=(-b+sqrt(b**2-(4*a*c)))/(2*a)
-        let x2=(-b-sqrt(b**2-(4*a*c)))/(2*a)
-    
-      console.log(` el valor de x1(raiz positiva)es igual a:`,x1)
-      console.log(` el valor de x2(raiz negativa) es igual a:`,x2)
-    
-                                                                                                                                                                                                                                                                                                                                                                                  
-    //salida o entrega de resultados
-      console.log(`el resultado re la raiz`)
+    let a = Number(prompt("Ingrese valor para a:"))
+    let b = Number(prompt("Ingrese valor para b:"))
+    let c = Number(prompt("Ingrese valor para c:"))
+
+
+    console.log(`El volumen de elipsoide es: ${(4/3)*(Math.PI)*a*b*c}`);
 })
