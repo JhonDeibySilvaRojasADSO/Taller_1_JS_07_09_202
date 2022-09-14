f62f42a4f8c97b334492dd528078ef398f1dc5cd
camilohm1 <cahernandez3510@misena.edu.co>
2022-09-14 08:40:24 -0500

    ejercicio16

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-addEventListener("DOMContentLoaded", ()=>{// definir variables numericas 
-// un programa 
-let grados=Number(prompt("grados centigrados"))
-
-// operaciones basicas ,*
-let f=(9/5)*grados+32
-
-console.log("fahrenheit :",f)
+addEventListener("DOMContentLoaded", ()=>{
+    // definir variables numericas 
+    // un programa 
+    let a=Number(prompt("nota 1 parcial:"))
+    let b=Number(prompt("nota 2 parcial:"))
+    let c=Number(prompt("nota 3 parcial:"))
+    let ex=Number(prompt("calificacion de examen final:"))
+    let fl=Number(prompt("trabajo final:"))
+    // operaciones basicas ,*
+    let pr=(a+c+b)/3
+    let fm=(pr*0.55)+(ex*0.30)+(fl*0.15)
+    console.log("promedio final :",fm)
 })
