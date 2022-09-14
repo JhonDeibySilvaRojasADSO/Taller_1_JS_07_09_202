04e7b50b809e0ea1f2c19024d3b37334bba96b34
camilohm1 <cahernandez3510@misena.edu.co>
2022-09-14 08:57:05 -0500

    ejercicio18

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-// definir variables numericas 
-// un programa 
-//17 VALOR DEL COSTO DEL PRODUCTO
-//Datos de entrada
-console.log("ingrese el nombre del primer producto:")
-let p1=console.log()
-let p1v=Number(prompt("valor del producto:"))
-
-console.log("ingrese el nombre del segundo  producto:")
-let p2=console.log()
-let p2v=Number(prompt("valor del producto:"))
-
-
-//procesos
-let Subttl = p1v+p2v
-let iva= Number(Subttl*0.19)
-let total= Number(Subttl + iva)
-//salidas o entrega de resultados
-console.log("el subtotal fue",Subttl)
-console.log("El iva fue",iva)
-console.log("El total a pagar con iva fue",total)
+    //datos de entrada 
+    let nom=prompt("coloque aqui el nombre del empleado:")
+    let hrt=Number(prompt("coloque aqui las horas de trabajo"))
+    // operacion basica
+    let vlp=15300
+    let suel=vlp*hrt
+    console.log("el sueldo es:",suel)
 })
