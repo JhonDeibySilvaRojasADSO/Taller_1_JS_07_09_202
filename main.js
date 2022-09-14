f8bb608e6a79feca0b4d1d8dc15eacdfb96e80f1
camilohm1 <cahernandez3510@misena.edu.co>
2022-09-14 09:01:14 -0500

    ejercicio19

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //datos de entrada 
-    let nom=prompt("coloque aqui el nombre del empleado:")
-    let hrt=Number(prompt("coloque aqui las horas de trabajo"))
-    // operacion basica
-    let vlp=15300
-    let suel=vlp*hrt
-    console.log("el sueldo es:",suel)
+    //datos de entrada
+    let nota1=Number(prompt("coloque aqui la primera nota"))
+    let nota2=Number(prompt("coloque aqui la segunda nota"))
+    let nota3=Number(prompt("coloque aqui la tercera nota"))
+    let nota4=Number(prompt("coloque aqui la cuarta nota"))
+    let suma=(nota1+nota2+nota3+nota4)/4
+    if (suma>3)
+      console.log("aprobado")
+    if (suma<3)
+      console.log("reprobado")
+    console.log("nota final;",suma)
 })
